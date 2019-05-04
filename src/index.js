244bedd7bd8062748cdccf60dcf28083c0a79aa7
Cristian SC <sifuentescovarrubiasitsl@gmail.com>
2019-05-04 01:07:42 -0500

    CSC 2019/05/04: Entendiendo graphql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import graphqlHTTP  from "express-graphql";
 
 console.log('Testing');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.json(
+     {
+       message : 'Hola!'
+     })
+});
+
+app.use('/graphql', graphqlHTTP ({
+
+}));
 
 app.listen(3000, () => {
   console.log('Estoy creando un sevridor');
